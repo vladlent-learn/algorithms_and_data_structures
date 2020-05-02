83f4edd85794eb0b39c3eca2be48e581ef250858
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2020-05-02 16:28:27 +0300

    added jasmine tests

---
+++ spec/data-structures/singly-linked-list.spec.ts
@@ -0,0 +1,8 @@
+import 'jasmine';
+import { SinglyLinkedList } from '../../data-structures/singly-linked-list';
+
+describe('SinglyLinkedList', () => {
+  it('should create', () => {
+    expect(new SinglyLinkedList()).toBeTruthy();
+  });
+});
